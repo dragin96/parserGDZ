b436b81b192f790d5bb9bc9f6e5bb8b744a48f16
chappa <wadaragorn@gmail.com>
2018-09-10 15:38:06 +0500

    stable

--- getTask.js
+++ getTask.js
@@ -1,10 +1,10 @@
 function getTask(urls) {
 	let tmpArr = [];
 	for (const key in urls) {
-		console.log("создать папку: " + key);
+		//console.log("создать папку: " + key);
 		const razdelTask = urls[key];
 		razdelTask.forEach(element => {
-			console.log(element.url);
+			//console.log(element.url);
 		});
 	}
 }
